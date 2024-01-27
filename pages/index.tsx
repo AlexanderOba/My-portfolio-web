37997ef29edc9378cb837c5d3aab75cb3e72e6a3
AlexanderOba <alexzy4ak@gmail.com>
2024-01-27 16:21:35 +0100

    changed resume link

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,7 @@ export default function Home() {
             can get in touch with me here.
           </p>
           <div>
-            <Link href="https://drive.google.com/file/d/1CRqoxNttsrePaTyRp1EHVnXxL6tA3jdX/view?usp=drive_link" legacyBehavior={true}>
+            <Link href="https://drive.google.com/file/d/17VwbQBxm2SBxQ1MHZRxMTNvSUgqAUYay/view?usp=drive_link" legacyBehavior={true}>
               <a target='_blank' >
                 <button className="btn button-1">View Resume  <Image src={exticon} alt="icon" className='img-fluid link_icon' width={15} height={15} /></button>
               </a>
